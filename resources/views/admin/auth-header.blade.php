7b268579841ea43e3a1e3af964c90cf3aebb9cc1
dipul@429820 <mddipulhasan@gmail.com>
2024-06-23 10:29:14 +0600

    update all files

--- resources/views/admin/auth-header.blade.php
+++ resources/views/admin/auth-header.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
   data-template="vertical-menu-template-free">
-
 <head>
   <meta charset="utf-8" />
   <meta name="viewport"
